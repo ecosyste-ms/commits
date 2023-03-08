bcdaf19ac9bd7e4a1be48832b52fba1eb5affdf1
Andrew Nesbitt <andrewnez@gmail.com>
2023-03-08 11:00:28 +0000

    Controller tests

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -27,10 +27,10 @@ class Api::V1::RepositoriesController < Api::V1::ApplicationController
     @repository = @host.repositories.find_by('lower(full_name) = ?', path.downcase)
     if @repository
       @repository.sync_async(request.remote_ip) unless @repository.last_synced_at.present? && @repository.last_synced_at > 1.day.ago
-      redirect_to repository_path(@repository)
+      redirect_to host_repository_path(@host, @repository)
     else
       @host.sync_repository_async(path, request.remote_ip) if path.present?
-      redirect_to repository_path(@repository)
+      redirect_to host_repository_path(@host, @repository)
       render json: { error: 'Repository not found' }, status: :not_found
     end
   end
