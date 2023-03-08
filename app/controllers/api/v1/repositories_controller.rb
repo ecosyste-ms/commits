f8d752eb6d07457b92228a512cd7c6f42dee3cdb
Andrew Nesbitt <andrewnez@gmail.com>
2023-03-08 10:54:38 +0000

    Finish API

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::RepositoriesController < Api::V1::ApplicationController
 
+  def index
+    @host = Host.find_by_name!(params[:host_id])
+    scope = @host.repositories
+    scope = scope.created_after(params[:created_after]) if params[:created_after].present?
+    scope = scope.updated_after(params[:updated_after]) if params[:updated_after].present?
+
+    if params[:sort].present? || params[:order].present?
+      sort = params[:sort] || 'last_synced_at'
+      order = params[:order] || 'desc'
+      sort_options = sort.split(',').zip(order.split(',')).to_h
+      scope = scope.order(sort_options)
+    else
+      scope = scope.order('last_synced_at DESC')
+    end
+
+    @pagy, @repositories = pagy_countless(scope)
+  end
+
   def lookup
     url = params[:url]
     parsed_url = Addressable::URI.parse(url)
--- app/views/api/v1/hosts/_host.json.jbuilder
+++ app/views/api/v1/hosts/_host.json.jbuilder
@@ -1,1 +1,3 @@
 json.extract! host, :name, :url, :kind, :last_synced_at, :repositories_count, :commits_count
+json.host_url api_v1_host_url(host)
+json.repositories_url api_v1_host_repositories_url(host)
