4662dde00085820b181e75405cd49cf13c203559
Andrew Nesbitt <andrewnez@gmail.com>
2023-04-27 11:50:38 +0100

    Config tweaks

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Diff
+module Commits
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
