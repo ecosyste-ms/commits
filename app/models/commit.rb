9b9ac5ba10644bff03e6766a8fea0057bc8b9aa5
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-12 17:43:51 +0000

    Add labelled_stats method to Commit model and include html_url and stats in commit JSON response

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -6,4 +6,28 @@ class Commit < ApplicationRecord
 
   scope :since, ->(date) { where('timestamp > ?', date) }
   scope :until, ->(date) { where('timestamp < ?', date) }
+
+  def labelled_stats
+    {
+      files_changed: files_changed,
+      additions: additions,
+      deletions: deletions
+    }
+  end
+
+  def files_changed
+    stats[0]
+  end
+
+  def additions
+    stats[1]
+  end
+
+  def deletions
+    stats[2]
+  end
+
+  def html_url
+    "#{repository.html_url}/commit/#{sha}"
+  end
 end
--- app/views/api/v1/commits/_commit.json.jbuilder
+++ app/views/api/v1/commits/_commit.json.jbuilder
@@ -1,1 +1,3 @@
 json.extract! commit, :sha, :message, :author, :committer, :timestamp, :merge
+json.html_url commit.html_url
+json.stats commit.labelled_stats
