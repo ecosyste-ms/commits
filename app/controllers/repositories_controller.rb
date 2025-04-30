eefd99cab853239339803b2a41b87b69ebca4cd5
Andrew Nesbitt <andrewnez@gmail.com>
2025-04-30 14:56:24 +0100

    Add missing presence check for @host in show action

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -40,6 +40,7 @@ class RepositoriesController < ApplicationController
 
   def show
     @host = Host.find_by_name!(params[:host_id])
+    raise ActiveRecord::RecordNotFound unless @host
     @repository = @host.repositories.find_by('lower(full_name) = ?', params[:id].downcase)
     fresh_when @repository, public: true
     if @repository.nil?
