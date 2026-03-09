e4f793b4e9754ff185014fcd2be38ebf1f23436a
Andrew Nesbitt <andrewnez@gmail.com>
2026-03-09 09:38:11 +0000

    Add repositories to committer API response and document endpoint in OpenAPI spec

--- app/views/api/v1/committers/show.json.jbuilder
+++ app/views/api/v1/committers/show.json.jbuilder
@@ -1,1 +1,6 @@
 json.extract! @committer, :id, :login, :emails, :commits_count, :created_at, :updated_at, :repositories_count
+
+json.repositories @contributions do |contribution|
+  json.extract! contribution.repository, :id, :full_name
+  json.commit_count contribution.commit_count
+end
--- test/controllers/api/v1/committers_controller_test.rb
+++ test/controllers/api/v1/committers_controller_test.rb
@@ -24,6 +24,17 @@ class Api::V1::CommittersControllerTest < ActionDispatch::IntegrationTest
       assert_equal 2, json["repositories_count"]
     end
 
+    should "return repositories ordered by commit count" do
+      get api_v1_host_committer_path(@host.name, @committer.login)
+      json = JSON.parse(response.body)
+      repos = json["repositories"]
+      assert_equal 2, repos.length
+      assert_equal @repo1.full_name, repos[0]["full_name"]
+      assert_equal 300, repos[0]["commit_count"]
+      assert_equal @repo2.full_name, repos[1]["full_name"]
+      assert_equal 200, repos[1]["commit_count"]
+    end
+
     should "redirect when finding by email" do
       get api_v1_host_committer_path(@host.name, @committer.emails.first)
       assert_response :moved_permanently
